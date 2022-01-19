77c1ea3804371a7465642fd8dc9f3eed1c00b066
duyngo0501 <97430123+duyngo0501@users.noreply.github.com>
2022-01-19 11:32:52 +0700

    you just have commit

---
+++ Exercise3/Candidate.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise3
+{
+    public class Candidate
+    {
+        private int iD;
+        private string name;
+        private string year;
+        private int math;
+        private int literature;
+        private int english;
+        public Candidate()
+        {
+
+        }
+        public Candidate(int iD,string name,string year,int math,int literature,int english)
+        {
+            this.iD = iD;
+            this.name = name;
+            this.year = year;
+            this.math = math;
+            this.literature = literature;
+            this.english = english;
+        }
+        public int ID
+        {
+            get { return iD; }
+            set { iD = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public int Math
+        {
+            get { return math; }
+            set { math = value; }
+        }
+        public int Literature
+        {
+            get { return literature; }
+            set { literature = value; }
+        }
+        public int English
+        {
+            get { return english; }
+            set { english = value; }
+        }
+        public void InputInfor()
+        {
+            Console.WriteLine("Input ID of student: ");
+            ID = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input name of student: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Input year of birth: ");
+            Year = Console.ReadLine();
+            Console.WriteLine("Input Math mark: ");
+            Math = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input Literature mark: ");
+            Literature = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input English mark: ");
+            English = int.Parse(Console.ReadLine());
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID of student: " + ID);
+            Console.WriteLine("Name of Student: " + Name);
+            Console.WriteLine("Year of Birth: " + Year);
+            Console.WriteLine("Math mark: " + Math);
+            Console.WriteLine("Literature mark: " + Literature);
+            Console.WriteLine("English mark: " + English);
+        }
+        public double Totalmark()
+        {
+            return (Math + Literature + English);
+        }
+    }
+}
---
+++ Exercise3/TestCandidate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class TestCandidate
+    {
+        static void Main(string[] args)
+        {
+            //Input n student
+            int n;
+            Console.WriteLine("Input the number student you want to input: ");
+            n = int.Parse(Console.ReadLine());
+            Candidate[] list = new Candidate[n];
+            for (int i = 0; i<n;i++)
+            {
+                Console.WriteLine("\nInput the information for student {0}:", i+ 1);
+                list[i] = new Candidate();
+                list[i].InputInfor();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nInformation of the Student {0}: ", i + 1);
+                list[i].Display();
+            }
+            //Find the student have a total mark > 15
+            for (int i = 0; i< n;i++)
+            {
+                if (list[i].Totalmark() > 15)
+                {
+                    Console.WriteLine("\nThis is a candidates with total score greater than 15: ");
+                    list[i].Display();
+                }
+            }
+        }
+    }
+}
